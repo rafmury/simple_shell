84af2812037972d74be754ba55961c688e5b95ff
RIDWAN FARHAN <ridubangire@gmail.com>
2023-05-25 01:14:08 +0300

    Update

--- linkedlist.c
+++ linkedlist.c
@@ -10,7 +10,7 @@ void free_list(list_t *head);
  * @head: A pointer to the head of the list_t list.
  * @name: The name of the new alias to be added.
  * @value: The value of the new alias to be added.
- *
+ * by @rafmury and @ridhwan497
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  */
